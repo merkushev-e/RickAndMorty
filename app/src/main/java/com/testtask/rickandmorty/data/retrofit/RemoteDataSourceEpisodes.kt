ef68951f3f81dda971638c0311b9cb70ce32f9e2
Egor M <merkushev.eg@gmail.com>
2022-06-29 20:04:08 +0200

    some changes with datasource

---
+++ app/src/main/java/com/testtask/rickandmorty/data/retrofit/RemoteDataSourceEpisodes.kt
@@ -0,0 +1,17 @@
+package com.testtask.rickandmorty.data.retrofit
+
+import com.testtask.rickandmorty.data.DataSource
+import com.testtask.rickandmorty.data.retrofit.model.EpisodeDTO
+import com.testtask.rickandmorty.data.retrofit.model.EpisodesResultDTO
+
+class RemoteDataSourceEpisodes(private val api: RMApi) :
+    DataSource<EpisodesResultDTO, EpisodeDTO> {
+
+    override suspend fun getDataByPages(page: Int): EpisodesResultDTO {
+        return api.getAllEpisode(page)
+    }
+
+    override suspend fun getDataById(id: Int): EpisodeDTO {
+        return api.getEpisodeById(id)
+    }
+}
---
+++ app/src/main/java/com/testtask/rickandmorty/domain/model/LocationData.kt
@@ -0,0 +1,11 @@
+package com.testtask.rickandmorty.domain.model
+
+data class LocationData(
+    val id: Int,
+    val name: String,
+    val type: String,
+    val dimension: String,
+    val residents: List<String>,
+    val url: String,
+    val created: String
+)
